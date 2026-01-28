9fb0f21712e23ce2446a21bd9789c88a2713a787
RainerToTheDowner <ryudragon2011@gmail.com>
2026-01-28 07:11:47 -0800

    Find Push Events

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+userinput = ""
+while userinput != "exit":
+    userinput = input()
+    if userinput == "exit":
+        break
+
+    userdata = (requests.get(f"https://api.github.com/users/{userinput}/events")).json()
+    totalPushesPerRepo = {}
+    for activity in userdata:
+        if activity["type"] == "PushEvent":
+            fullrepolink = activity["repo"]["url"]
+            repo = fullrepolink.split("/")[-2:]
+            repo = "/".join(repo)
+            try:
+                totalPushesPerRepo[repo] += 1
+            except:
+                totalPushesPerRepo[repo] = 1
+    for repo in list(totalPushesPerRepo.keys()):
+        print("Pushed " + str(totalPushesPerRepo[repo]) + " commits to " + repo)
